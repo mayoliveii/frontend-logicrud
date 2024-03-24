b5b85b983c2a83bbde25127ecb341203a4a4456f
mayoliveii <mayoliveirablv@gmail.com>
2024-03-24 14:53:16 -0300

    chore: add link to images in s3

---
+++ src/components/Vehicle/images-s3.ts
@@ -0,0 +1,13 @@
+export const IMAGE_URL_S3 = [
+  'logistic-image-1.jpg',
+  'logistic-image-2.jpg',
+  'logistic-image-3.jpg',
+  'logistic-image-4.jpg',
+  'logistic-image-5.jpg',
+  'logistic-image-6.jpg',
+  'logistic-image-7.jpg',
+  'logistic-image-8.jpg',
+  'logistic-image-9.jpg',
+  'logistic-image-10.jpg',
+  'logistic-image-11.jpg',
+];
